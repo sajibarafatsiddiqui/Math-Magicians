6d05d0a40e24297a49b20ebc7fe5f45026a1e15e
sajibarafatsiddiqui <siddiquisajib75@gmail.com>
2023-02-15 20:53:26 +0600

    Applied necessary styling to make it looks real calculator

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -2,7 +2,7 @@ import Key from './Key';
 
 const buttons = ['AC', '+/-', '%', '÷', '7', '8', '9', '×', '4', '5', '6', '-', '1', '2', '3', '+', '0', '.', '='];
 const Calculator = () => (
-  <section>
+  <section className="container">
     <Key clname="key darkgray" char="0" />
     <div className="keyContainer">{buttons.map((val) => (<Key key={val} clname="key lightgray" char={val} />))}</div>
   </section>
