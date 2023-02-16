39b0aba1915ab33e1dbd31a28a49b177eb6b275d
sajibarafatsiddiqui <siddiquisajib75@gmail.com>
2023-02-16 23:58:36 +0600

    Solved type warning of display component

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -6,15 +6,15 @@ import calculate from '../logic/calculate';
 const buttons = ['AC', '+/-', '%', '÷', '7', '8', '9', 'x', '4', '5', '6', '-', '1', '2', '3', '+', '0', '.', '='];
 const Calculator = () => {
   const [tools, setTools] = useState({ total: 0, next: 0, operation: null });
-  const [display, setDisplay] = useState(0);
+  const [display, setDisplay] = useState('0');
   useEffect(() => {
     if (tools.next) {
-      setDisplay(tools.next);
+      setDisplay(tools.next.toString());
     } else if (tools.operation) {
-      setDisplay(tools.operation);
+      setDisplay(tools.operation.toString());
     } else if (tools.total) {
-      setDisplay(tools.total);
-    } else setDisplay(0);
+      setDisplay(tools.total.toString());
+    } else setDisplay('0');
   }, [tools.next, tools.operation, tools.total]);
   const operation = (e) => {
     const updatedTools = calculate(tools, e.target.innerHTML);
