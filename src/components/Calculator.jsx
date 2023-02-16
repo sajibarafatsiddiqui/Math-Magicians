56f23442aab0aa793246cd0ef4d8f2c11e540fec
Sajib Arafat Siddiqui <sajib.arafat@nrbglobalbank.com>
2023-02-16 17:10:48 +0600

    Added onclick props at Key component and added handler at parent component

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -1,11 +1,22 @@
+import React, { useState } from 'react';
 import Key from './Key';
+import Display from './Display';
+import calculate from '../logic/calculate';
 
 const buttons = ['AC', '+/-', '%', '÷', '7', '8', '9', '×', '4', '5', '6', '-', '1', '2', '3', '+', '0', '.', '='];
-const Calculator = () => (
-  <section className="container">
-    <Key clname="key darkgray" char="0" />
-    <div className="keyContainer">{buttons.map((val) => (<Key key={val} clname="key lightgray" char={val} />))}</div>
-  </section>
-);
-
+const Calculator = () => {
+  const [tools, setTools] = useState({ total: null, next: null, operation: null });
+  const operation = (e) => {
+    setTools(calculate(tools, e.target.innerHTML));
+    console.log(tools.total);
+  };
+  return (
+    <section className="container">
+      <Display clname="key darkgray" char="0" />
+      <div className="keyContainer">{buttons.map((val) => (<Key key={val} clname="key lightgray" char={val} handleClick={operation} />))}</div>
+    </section>
+  );
+};
 export default Calculator;
+//
+// return (
